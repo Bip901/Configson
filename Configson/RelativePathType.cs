b35aeea4c190dcec952e648540afb491ef20f643
Bip901 <74911767+Bip901@users.noreply.github.com>
2023-07-08 12:14:11 +0300

    Add project files

---
+++ Configson/RelativePathType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configson
+{
+    public enum RelativePathType
+    {
+        /// <summary>
+        /// Paths are relative to the process working directory.
+        /// </summary>
+        RelativeToCurrentWorkingDirectory = 0,
+        /// <summary>
+        /// Paths are relative to the app's directory (typically the directory where the main executable is stored)
+        /// </summary>
+        RelativeToAppBaseDirectory,
+        /// <summary>
+        /// Paths are relative to the system's %appdata% (XDG_CONFIG_HOME) folder. On Windows, that's usually ~/AppData/Roaming. On Linux, usually ~/.config.
+        /// </summary>
+        RelativeToAppData
+    }
+}
